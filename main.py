6fe81ccedd16a60feb9afd7ec0865ee5e703d5de
Sunny <sunrajpatelcph@gmail.com>
2024-03-28 00:15:01 +0530

    Developed camera RNG

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import cv2 
 #features to be included:
 #true random number generator
 #pseudo random number generator
@@ -7,6 +8,44 @@
 #file decryption
 
 
+
+#true random number generator
+def true_randint(lower, upper, method = None):
+    if method == None:
+        #raise error
+        raise Exception("No method found")
+    if lower >= upper:
+        raise Exception("Range of numbers is wrong")
+    if method == 'camera':
+        #get frame
+        vid = cv2.VideoCapture(0) 
+        ret, frame = vid.read()
+        vid.release()
+
+        #choose central pixel
+        central_pixel = frame[int(len(frame)/2)][0]
+        # print(central_pixel)
+        m = 1
+        while m <= 10000 and (m!=0 and m!=1):
+            print(m)
+            for value in central_pixel:
+                m *= value
+        # print(m) 
+        #m holds a true random number
+        
+        normalized_m = m
+        while normalized_m > 10000:
+            normalized_m %= 10
+        normalized_m /= 10000
+
+
+        return int((normalized_m * (upper-lower)) + lower)
+
+    elif method == 'mic':
+        #use mic for generation 
+        return number
+    else:
+        raise Exception("Invalid method")
 
 #data encryption
 def encrypt(text, method = "caesar_cipher", shift = None):
@@ -21,7 +60,7 @@ def encrypt(text, method = "caesar_cipher", shift = None):
         return ret
 
     
-
+#data decryption
 def decrypt(text, method = "caesar_cipher", shift = None):
     if method == "caesar_cipher":
         ret = ""
@@ -31,6 +70,7 @@ def decrypt(text, method = "caesar_cipher", shift = None):
             ret += chr(ord(alpha)-shift)
         return ret
 
-    
-print(encrypt("this is a test!"))
-print(decrypt("wklv#lv#d#whvw$"))
+
+if __name__ == "__main__":    
+    print(encrypt("this is a test!"))
+    print(decrypt("wklv#lv#d#whvw$"))
---
+++ test.py
@@ -0,0 +1,3 @@
+from main import *
+
+print(true_randint(1, 10, "camera"))
